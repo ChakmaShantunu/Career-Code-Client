c335836de6b14cdef8b85270f2fdc2155fcb0e85
shantunu007 <chakmashantunu.web@gmail.com>
2026-08-07 18:10:10 +0600

    setup root layout and home route

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,11 +1,18 @@
 import { createBrowserRouter } from "react-router";
 import { } from "react-router/dom";
 import RootLayout from "../layouts/RootLayout";
+import Home from "../pages/Home/Home";
 
 const router = createBrowserRouter([
     {
         path: "/",
         Component: RootLayout,
+        children: [
+            {
+                index: true,
+                Component: Home
+            }
+        ]
     },
 ]);
 
